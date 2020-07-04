dfff520e81b03a39cb8a836147d21a850fbdedbf
PranavKharbade <pranav.kharbade@gmail.com>
2020-07-05 00:28:25 +0530

    dynamic routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom'
 
-const MenuItem=({title,imageUrl,size})=>(
+const MenuItem=({title,imageUrl,size,history,match,linkUrl})=>(
     <div 
     style={{
         backgroundImage:`url(${imageUrl})`
     }} 
     className={`${size} menu-item`}
+    onClick={()=>history.push(`${match.url}${linkUrl}`)}
     >
         <div className='background-image'style={{
         backgroundImage:`url(${imageUrl})`
@@ -19,4 +21,4 @@ const MenuItem=({title,imageUrl,size})=>(
             </div>
     )
 
-export default MenuItem;
+export default withRouter(MenuItem);
